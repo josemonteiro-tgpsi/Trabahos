646ec30293b38835ba7e5bc0f30587aa8129a54e
josemonteiro-tgpsi <jose.monteiro.127028@epge.pt>
2025-10-02 09:50:04 +0100

    Create Anosdevida.cs

---
+++ src/Anosdevida.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int anoDias = 365;
+            int idade, totalEmDias;
+
+
+            Console.Write("Tens quantos anos?: ");
+            idade = Convert.ToInt16(Console.ReadLine());
+
+            totalEmDias = 365 * idade;
+
+            Console.WriteLine($"Tens {totalEmDias} anos de vida.");
+
+        }
+    }
+}
