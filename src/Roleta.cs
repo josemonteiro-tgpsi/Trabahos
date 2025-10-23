2beb3dbd4068557c7dbaf51b7fbe7f9078bb2e4c
josemonteiro-tgpsi <jose.monteiro.127028@epge.pt>
2025-10-23 09:45:05 +0100

    Create Roleta.cs

---
+++ src/Roleta.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Tasks.Sources;
+
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int numeroC = 365;
+            int numeroE;
+            Console.Write("Tenta adivinhar o numero (0-999): ");
+            numeroE = Convert.ToInt16(Console.ReadLine());
+
+            if (numeroE < 0 || numeroE > 999)
+            {
+                Console.WriteLine("[!] Erro: O número não pode ser maior que 999 ou menor que 0.");
+            } else if (numeroE == numeroC) {
+                Console.WriteLine("[+] Acertaste o número!");
+            } else { 
+                Console.WriteLine("[-] Não acertaste o número.");
+            }
+        }
+    }
+}
