56b983d7d10975e114eb368e78eec9a9a186df13
josemonteiro-tgpsi <jose.monteiro.127028@epge.pt>
2025-11-25 08:52:59 +0000

    Update MasterMinds.cs

--- src/MasterMinds.cs
+++ src/MasterMinds.cs
@@ -2,34 +2,65 @@ using System;
 
 namespace program
 {
-    class Program
+    class Program   
     {
-
-        void CheckPos(string s)
-        {
-            for (int i = 0; i <= 4; i++)
-            {
-                if (shoot[i] == n1)
-                {
-
-                }
-            }
-        }
-
         static void Main()
         {
             string shoot;
+            int tentativas = 4;
             Random r = new Random();
+            int c = 0;
+            bool C_pos;
+            bool C_num;
 
             int n1 = r.Next(1, 9);
             int n2 = r.Next(1, 9);
             int n3 = r.Next(1, 9);
             int n4 = r.Next(1, 9);
 
+            int t1, t2, t3, t4;
+
             Console.WriteLine($"[*] Bem vindo ao Master Minds");
-            Console.Write(": ");
-            shoot = Console.ReadLine();
-            
+            Console.WriteLine("[+] Tens quatro tentativas");
+
+            while (tentativas >= 1)
+            {
+                Console.WriteLine($"Tentativa {tentativas}");
+
+                Console.Write("Numero 1: ");
+                t1 = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine();
+                Console.Write("Numero 2: ");
+                t2 = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine();
+                Console.Write("Numero 3: ");
+                t3 = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine();
+                Console.Write("Numero 4: ");
+                t4 = Convert.ToInt16(Console.ReadLine());
+                
+                
+                if (t1 == n1 && t2 == n2 && t3 == n3 && t4 == n4)
+                   Console.WriteLine("Acertaste o código!");
+                
+                if (t1 == n1)
+                    C_pos = true;
+                    C_num = true;
+                
+                if (t2 == n2)
+                    C_pos = true;
+                    C_num = true;
+                    
+                if (t3 == n3)
+                    C_pos = true;
+                    C_num = true;
+                
+                if (t4 == n4)
+                    C_pos = true;
+                    C_num = true;
+                    
+                tentativas--;
+            }
         }
     }
 }
